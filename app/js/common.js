dc809eac34e017154d3af8486ec52b7652092e39
StarkOne <Stark.pro40@gmail.com>
2017-02-18 20:47:28 +0700

    section-3

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,9 @@ $(function() {
 	$(".toggle-mnu").click(function(){
 		$(this).toggleClass("on");
 		$(".main-mnu").slideToggle();
-	})
+	});
+
+	$(".section-content .info-item").equalHeights();
+	$(".s1-bottom info-item").equalHeights();
 
 });
