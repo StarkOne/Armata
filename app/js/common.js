31e38cc6b0c29670be93f2100b3008105ccd3b72
StarkOne <Stark.pro40@gmail.com>
2017-02-19 19:55:24 +0700

    section 5

--- app/js/common.js
+++ app/js/common.js
@@ -8,5 +8,19 @@ $(function() {
 	$(".section_1 .section-content .info-item").equalHeights();
 	$(".section_3 .section-content .info-item").equalHeights();
 	$(".s1-bottom .info-item").equalHeights();
+	$(".s2-item").equalHeights();
+	$(".s2-bottom .img-wrap").equalHeights();
+	//$(".cards .card").equalHeights();
 	
+	$(".section_4 ").waypoint(function() {
+
+		$(".section_4 .card").each(function(index){
+			var ths = $(this);
+			setInterval(function(){
+				ths.removeClass("card-off").addClass("card-on");
+			}, 150*index);
+		});
+	},{
+		offset:"20%"
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/equaH/jquery.equalheights.min.js',
+		'app/libs/waypoints/waypoints.min.js',
 		'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
